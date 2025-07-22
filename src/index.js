745d91bc266f264aeeb796d9dd9c476f2bd37f5b
dafaque <ihmiwtd@ya.ru>
2025-07-22 20:01:59 +0300

    initial

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export { default as App } from "./app.js";
+export { default as Router } from "./router.js";
+export { default as Form } from "./views/form.js";
+export { default as Menu } from "./views/menu.js";
+export { default as Table } from "./views/table.js";
+export { default as View } from "./views/view.js";
---
+++ src/views/select-view.js
@@ -0,0 +1,53 @@
+import View from "./view.js";
+import Menu from "./menu.js";
+
+export default class SelectView extends View {
+    constructor() {
+        super();
+        this.menu = null;
+        this.onSave = null;
+    }
+
+    renderContent() {
+        const container = document.createElement('div');
+        container.className = 'select-view';
+        
+        const field = this.data?.field;
+        const currentValue = this.data?.currentValue;
+        
+        if (!field || !field.choices) {
+            const errorMessage = document.createElement('div');
+            errorMessage.className = 'error-message';
+            errorMessage.textContent = 'No choices available';
+            container.appendChild(errorMessage);
+            return container;
+        }
+        
+        // Создаем меню из choices
+        const menuOptions = field.choices.map(choice => ({
+            label: choice.text || choice.label,
+            action: () => {
+                if (this.data?.onSave) {
+                    this.data.onSave(choice.value);
+                }
+                this.goBack();
+            }
+        }));
+        
+        this.menu = new Menu(menuOptions);
+        const menuElement = this.menu.render();
+        container.appendChild(menuElement);
+        
+        return container;
+    }
+
+    onKeyDown(e) {
+        if (this.menu) {
+            this.menu.onKeyDown(e);
+        }
+    }
+
+    cacheable = () => {
+        return false;
+    }
+}
