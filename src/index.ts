13a58ea2867abfda6c8f631016bb605e724c883b
dafaque <ihmiwtd@ya.ru>
2025-08-13 21:23:20 +0300

    fix type decl

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-export { default as App } from "./app";
+import App from "./app";
+export { App };
 export { Form, FieldType } from "./views/form";
 export { default as Menu } from "./views/menu";
 export { default as Table } from "./views/table";
 export { default as View } from "./views/view";
 export { default as SelectView } from "./views/select-edit";
 export { default as TextView } from "./views/text-edit";
+
+
+declare global {
+    interface Window {
+        app: App;
+    }
+}
--- src/types/global.d.ts
+++
@@ -1,9 +0,0 @@
-import App from "../app";
-
-declare global {
-  interface Window {
-    app: App;
-  }
-}
-
-export {};
